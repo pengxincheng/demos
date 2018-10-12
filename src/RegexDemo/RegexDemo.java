4a7e1fc649872e8ebdd37ce66a8ca2a76d350142
彭新成 <pengxincheng@ipaynow.cn>
2018-10-12 19:18:37 +0800

    正则识别金额

---
+++ src/RegexDemo/RegexDemo.java
@@ -0,0 +1,25 @@
+package RegexDemo;
+
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author pengxincheng@ipaynow.cn
+ * @Date: 2018/10/12
+ * @Time 17:20
+ */
+public class RegexDemo {
+
+    @Test
+    public void test1(){
+        String s = "dfgfdgfdg20000.96fgfdgfg";
+        Pattern pattern = Pattern.compile("(\\d+(\\.\\d+)?)");
+
+       Matcher matcher =  pattern.matcher(s);
+        if (matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
+}
