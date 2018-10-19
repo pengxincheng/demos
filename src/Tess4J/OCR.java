6644267f2d22d28d84383831e14a8f2f2d9c3963
彭新成 <pengxincheng@ipaynow.cn>
2018-10-19 19:56:08 +0800

    Tesseract-OCR 示例

--- src/Tess4J/OCR.java
+++ src/Tess4J/OCR.java
@@ -12,42 +12,47 @@ import java.io.File;
  * @Time 17:50
  */
 public class OCR {
-        /**
-         *
-         * @param srImage 图片路径
-         * @param ZH_CN 是否使用中文训练库,true-是
-         * @return 识别结果
-         */
-        public static String FindOCR(String srImage, boolean ZH_CN) {
-            try {
-                System.out.println("start");
-                double start=System.currentTimeMillis();
-                File imageFile = new File(srImage);
-                if (!imageFile.exists()) {
-                    return "图片不存在";
-                }
-                BufferedImage textImage = ImageIO.read(imageFile);
-                Tesseract instance=new Tesseract();
-                instance.setDatapath("C:\\Program Files (x86)\\Tesseract-OCR\\tessdata");//设置训练库
-                if (ZH_CN)
-                   instance.setLanguage("normal");//自己训练的字库
-                //instance.setLanguage("chi_sim");//中文字库
-                String result = null;
-                result = instance.doOCR(textImage);
-                double end=System.currentTimeMillis();
-                System.out.println("耗时"+(end-start)/1000+" s");
-                return result;
-            } catch (Exception e) {
-                e.printStackTrace();
-                return "发生未知错误";
+    /**
+     * @param srImage  图片路径
+     * @param language 字库
+     * @return 识别结果
+     */
+    public static String FindOCR(String srImage, String language) {
+        try {
+            System.out.println("start");
+            double start = System.currentTimeMillis();
+            File imageFile = new File(srImage);
+            if (!imageFile.exists()) {
+                return "图片不存在";
             }
-        }
-        public static void main(String[] args) throws Exception {
-            String result=FindOCR("D:\\testOcr\\test2.png",true);
-            System.out.println(result);
+            BufferedImage textImage = ImageIO.read(imageFile);
+            Tesseract instance = new Tesseract();
+            instance.setDatapath("C:\\Program Files (x86)\\Tesseract-OCR\\tessdata");//设置训练库
+            instance.setLanguage(language);
 
-            String result2=FindOCR("D:\\testOcr\\test1.png",true);
-            System.out.println(result2);
+            String result = null;
+            result = instance.doOCR(textImage);
+            double end = System.currentTimeMillis();
+            System.out.println("耗时" + (end - start) / 1000 + " s");
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "发生未知错误";
         }
     }
+
+    public static void main(String[] args) throws Exception {
+        String result = FindOCR("D:\\testOcr\\test2.png", "normal");
+        System.out.println(result);
+
+        String result2 = FindOCR("D:\\testOcr\\test1.png", "chi_sim");
+        System.out.println(result2);
+
+        String result3 = FindOCR("D:\\testOcr\\test3.png", "normal");
+        System.out.println(result3);
+
+        String result4 = FindOCR("D:\\testOcr\\test6.png", "eng");
+        System.out.println(result4);
+    }
+}
 
